4fa2b888a54b9fcfa10cfa971744eddff972171d
hridyansh5492 <hridyanshsati4321@gmail.com>
2025-04-14 17:58:30 +0530

    some changes

---
+++ home.js
@@ -0,0 +1,4 @@
+function play()
+{
+    window.location.href = "CHOOSEPLAYER.HTML";
+}
--- signin.js
+++ signin.js
@@ -2,7 +2,7 @@ function validate()
 {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if(username == "admin" && password == "admin")
+    if(username === "admin" && password === "admin")
     {
         alert("Sign In Sucessfully.");
         window.location.href = "HOME PAGE.HTML";
