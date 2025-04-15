4c4a525b09dd1e9f43834d76204067914dffdbf4
hridyansh5492 <hridyanshsati4321@gmail.com>
2025-04-15 21:15:10 +0530

    done adding 2player

---
+++ js/friends.js
@@ -0,0 +1,14 @@
+function p2()
+{
+    window.location.href = "2player.html";
+}
+
+function p3()
+{
+    window.location.href = "3player.html";
+}
+
+function p4()
+{
+    window.location.href = "4player.html";
+}
