8cc5c61ef524dad923429d1c755324cecdb85b8f
hridyansh5492 <hridyanshsati4321@gmail.com>
2025-04-15 21:33:34 +0530

    connected botplayer

--- js/choose.js
+++ js/choose.js
@@ -2,6 +2,10 @@ function back()
 {
     window.location.href = "homepage.html";
 }
+function bot()
+{
+    window.location.href = "botplayer.html";
+}
 function friends()
 {
     window.location.href = "friends.html";
